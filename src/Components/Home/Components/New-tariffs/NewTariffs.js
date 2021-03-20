1c19089ee7bd5b9a907719f05ba9234c314f8d55
Danila <danilapayne@gmail.com>
2021-03-20 11:30:07 +0500

    asd

--- src/Components/Home/Components/New-tariffs/NewTariffs.js
+++ src/Components/Home/Components/New-tariffs/NewTariffs.js
@@ -1,13 +1,17 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import "./New_tariffs.css";
+import zor_2 from './Images/zor_3.webp';
+import zor_10 from './Images/zor_10.webp';
+import zor_6 from './Images/zor_7.webp';
+import zor_15 from './Images/zor_15.webp';
 
 function New_tariffs() {
   const tariffs = [
     {
       id: 0,
       tariffName: `zo'r 10`,
-      tariffImg: "https://beeline.uz/binaries/content/gallery/zor_10.png",
+      tariffImg: zor_10,
       tariffDescription: {
         cost: "50 000",
         minutes: <i className="fas fa-infinity"> </i>,
@@ -18,7 +22,7 @@ function New_tariffs() {
     {
       id: 1,
       tariffName: `zo'r 15`,
-      tariffImg: "https://beeline.uz/binaries/content/gallery/zor_15.png",
+      tariffImg: zor_15,
       tariffDescription: {
         cost: "70 000",
         minutes: <i className="fas fa-infinity"> </i>,
@@ -29,7 +33,7 @@ function New_tariffs() {
     {
       id: 2,
       tariffName: `zo'r 2`,
-      tariffImg: "https://beeline.uz/binaries/content/gallery/zor_2.png",
+      tariffImg: zor_2,
       tariffDescription: {
         cost: "20 000",
         minutes: "2 000",
@@ -40,7 +44,7 @@ function New_tariffs() {
     {
       id: 3,
       tariffName: `zo'r 6`,
-      tariffImg: "https://beeline.uz/binaries/content/gallery/zor_6.png",
+      tariffImg: zor_6,
       tariffDescription: {
         cost: "35 000",
         minutes: <i className="fas fa-infinity"> </i>,
