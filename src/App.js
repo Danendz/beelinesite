6e2b9b11ae273f23d22a1064473ae16abf513636
Danila <danilapayne@gmail.com>
2021-03-22 16:44:06 +0500

    ad

--- src/App.js
+++ src/App.js
@@ -1,29 +1,15 @@
-import React from "react";
+import React, {Suspense} from "react";
 import Nav from "./Components/Nav/Nav";
-<<<<<<< HEAD
 import Home from './Components/Home/Home';
-=======
-import Home from "./Components/Home/Home";
->>>>>>> parent of 448807c (asd)
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import loadable from "@loadable/component";
 
-<<<<<<< HEAD
 const Cabinet = React.lazy(() => import("./Components/Cabinet/Cabinet"));
 
 const Search = React.lazy(() => import("./Components/Search/Search"));
 const Help = React.lazy(() => import("./Components/Help/Help"));
 const Tariff = React.lazy(() => import("./Components/Tariff/Tariff"));
-=======
-const Cabinet = loadable(() => import("./Components/Cabinet/Cabinet"));
-
-const Search = loadable(() =>
-  import("./Components/Search/Search")
-);
-const Help = loadable(() => import("./Components/Help/Help"));
-const Tariff = loadable(() => import("./Components/Tariff/Tariff"));
->>>>>>> parent of 448807c (asd)
 
 function App() {
   return (
@@ -31,7 +17,6 @@ function App() {
       <Nav />
       <div className="App container">
         <Switch>
-<<<<<<< HEAD
 
             <Route path="/" exact component={Home} />
           <Suspense fallback={<span>cabinet...</span>}>
@@ -46,13 +31,6 @@ function App() {
           <Suspense fallback={<span>cabinet...</span>}>
             <Route path="/tariffs" exact render={() => <Tariff />} />
           </Suspense>
-=======
-          <Route path="/" exact component={Home} />
-          <Route path="/cabinet" exact component={Cabinet} />
-          <Route path="/search" exact component={Search} />
-          <Route path="/help" exact component={Help} />
-          <Route path="/tariffs" exact component={Tariff} />
->>>>>>> parent of 448807c (asd)
         </Switch>
       </div>
     </Router>
