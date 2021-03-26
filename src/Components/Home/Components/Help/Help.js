fbddb4793adb4040378216aa3f1fc7dbdcbc2706
Danila <danilapayne@gmail.com>
2021-03-26 17:41:46 +0500

    help

---
+++ src/Components/Home/Components/Help/Help.js
@@ -0,0 +1,61 @@
+import React from "react";
+import "./Help.css";
+
+import { NavLink } from "react-router-dom";
+function Help() {
+  const help = [
+    {
+      help_img:
+        "https://beeline.uz/binaries/content/gallery/mainsite/icons/tariffs-and-services.svg",
+      help_description: "Управляй своей связью, не выходя из",
+      help_link: "Личного кабинета",
+      help_page: "cabinet",
+    },
+    {
+      help_img:
+        "https://beeline.uz/binaries/content/gallery/mainsite/icons/support.svg",
+      help_description: "",
+      help_link: "Мы всегда на связи с вами",
+      help_page: "help",
+    },
+    {
+      help_img:
+        "https://beeline.uz/binaries/content/gallery/mainsite/icons/info.svg",
+      help_description: "",
+      help_link: "Все новости",
+      help_page: "news",
+    },
+    {
+      help_img:
+        "https://beeline.uz/binaries/content/gallery/mainsite/icons/money.svg",
+      help_description: "",
+      help_link: "Пополняй баланс",
+      help_page: "pay",
+    },
+    {
+      help_img:
+        "https://beeline.uz/binaries/content/gallery/mainsite/icons/phone.svg",
+      help_description: "Управляй своей связью, не выходя из",
+      help_link: "iOS, ",
+      help_link2: "Android",
+      help_page: "Apps",
+    },
+  ];
+  return (
+    <div className="help_container_main">
+      {help.map((el) => (
+        <NavLink className="help_item" to={el.help_page}>
+          <div className="help_item_container">
+            <img src={el.help_img} alt={el.help_description} />
+            <span>
+              {el.help_description} {el.help_link}
+              {el.help_link2}
+            </span>
+          </div>
+        </NavLink>
+      ))}
+    </div>
+  );
+}
+
+export default Help;
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,13 +1,14 @@
 import React from "react";
 import "./Home.css";
-import Slider from "./Components/Slider/Slider"
+import Slider from "./Components/Slider/Slider";
 import Section from "./Components/Section-1/Section";
 import NewTariffs from "./Components/New-tariffs/NewTariffs";
-import Section2 from './Components/Section-2/Section_2'
+import Section2 from "./Components/Section-2/Section_2";
 import PopularServices from "./Components/Popular_services/PopularServices";
 import ShopHome from "./Components/Shop_home/ShopHome";
-import NewPromotions from './Components/NewPromotions/NewPromotions';
-import News from './Components/News/News';
+import NewPromotions from "./Components/NewPromotions/NewPromotions";
+import News from "./Components/News/News";
+import Help from "./Components/Help/Help";
 function Home() {
   return (
     <div className="home">
@@ -19,6 +20,7 @@ function Home() {
       <ShopHome />
       <NewPromotions />
       <News />
+      <Help />
     </div>
   );
 }
